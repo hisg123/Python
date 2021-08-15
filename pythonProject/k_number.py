48798345ec2d6021a72a0e850667b1c9524a3545
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-15 12:21:21 +0900

    k_number.py fixed
    plus best_operation code(by mark)

--- pythonProject/k_number.py
+++ pythonProject/k_number.py
@@ -3,18 +3,12 @@ def solution(array, commands):
     for command in commands:
         i, j, k = command[0], command[1], command[2]
         slice=array[i-1:j] #j는 -1 안해주는 이유는 끝에 하나를 안쳐준다.
-        #slice.sort()
-        sort(slice)
+        slice.sort()
         answer.append(slice[k-1])
-    print(answer)
     return answer
 
-def sort(array):
-    for i in range(0, len(array)): #range는 0~len(array-1)까지 뽑음 0, 1, 2, 3
-        for j in range(i, len(array)):
-            if array[i] > array[j]:
-                array[i], array[j] = array[j], array[i]
-    return array
+# def solution(array, commands):
+#     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
 if __name__ == '__main__':
     solution([1,5,2,6,3,7,4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
