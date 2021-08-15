d3a1338565800f18d9a95cc24eb1b6db3432cbf4
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-15 13:45:44 +0900

    printer.py fixed
    plus best_oper code

--- pythonProject/printer.py
+++ pythonProject/printer.py
@@ -2,7 +2,6 @@ def solution(priorities, location):
     cnt = 0
 
     while(len(priorities)!=0):
-        print(priorities, location)
         if priorities[0] != max(priorities) and location != 0:
             priorities.append(priorities.pop(0))
             location = location - 1
@@ -18,10 +17,21 @@ def solution(priorities, location):
 
         if priorities[0] == max(priorities) and location == 0:
             answer = location+1+cnt
-            print(answer)
             return answer
+
+# def solution2(priorities, location):
+#     queue =  [(i,p) for i,p in enumerate(priorities)]
+#     answer = 0
+#     while True:
+#         cur = queue.pop(0)
+#         if any(cur[1] < q[1] for q in queue):
+#             queue.append(cur)
+#         else:
+#             answer += 1
+#             if cur[0] == location:
+#                 return answer
 
 if __name__ == '__main__':
     solution([2,1,3,2], 2)
-    solution([1,1,9,1,1,1], 0)
+
 
