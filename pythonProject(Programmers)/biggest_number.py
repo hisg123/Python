c94dc5408a22d9e11756951140a7ccf589b03f60
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-17 20:36:14 +0900

    Update biggest_number.py

--- pythonProject(Programmers)/biggest_number.py
+++ pythonProject(Programmers)/biggest_number.py
@@ -1,22 +1,13 @@
-from itertools import permutations
-
 def solution(numbers):
-    answer_temp = ''
-    temp_list = list(permutations(numbers, len(numbers)))
-    answer_list = []
+    #0. key point
+    numbers_str = [str(num) for num in numbers]                 #1. 사전 값으로 정렬하기
+    numbers_str.sort(key=lambda num: num*3, reverse=True)       #2. number는 1000이하의 숫자이므로 x3(반복)한 값으로 비교
 
-    for temp in temp_list:
-        # print(temp, temp[0], len(temp))
-        answer_temp = ''
-        for i in range(len(temp)):
-            answer_temp = answer_temp+str(temp[i])
-        answer_list.append(int(answer_temp))
-        print(answer_list)
-
-    answer = str(max(answer_list))
-    print('this is', answer)
-    return answer
+    return str(int(''.join(numbers_str)))
+    # 만약 numbers=[0,0,0,0] 이라면 0 이 나와야 한다.
+    # join한 값을 int로 만들어 준 후 원하는 return값이 str이기 때문에 다시 str로 변환한다.
 
 if __name__ == '__main__':
     solution([6, 10, 2])
     solution([3, 30, 34, 5, 9])
+    solution([0,0,0])
