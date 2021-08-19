47dec3bed6786a49342d12e764a9d47b18672937
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-19 20:31:25 +0900

    not completed function_develop.py
    not yet

---
+++ pythonProject(Programmers)/function_develop.py
@@ -0,0 +1,40 @@
+def solution(progresses, speeds):
+    answer = []
+    temp_list = []
+
+    ##calculate distribution date
+    for i in range(len(progresses)):
+        temp_list.append(100-progresses[i])
+        if temp_list[i] % speeds[i] != 0:
+            temp_list[i] = temp_list[i]//speeds[i]
+            temp_list[i] +=1
+
+        else:
+            temp_list[i] = temp_list[i] // speeds[i]
+
+    print(temp_list)
+
+    cnt = 0
+    max_temp = temp_list[0]
+    i = 0
+    while(i!=len(temp_list)):
+        if temp_list[i] <= max_temp:
+            cnt += 1
+            print("<=",i ,max_temp, temp_list[i], cnt)
+
+        if temp_list[i] > max_temp:
+            max_temp = temp_list[i]
+            answer.append(cnt)
+            print(i, max_temp, temp_list[i] ,cnt)
+            cnt = 0
+
+
+        i += 1
+
+
+    print(answer)
+    return answer
+
+if __name__ == '__main__':
+    solution([93,30,55],[1,30,5])
+    solution([95, 90, 99, 99, 80, 99], 	[1, 1, 1, 1, 1, 1])
