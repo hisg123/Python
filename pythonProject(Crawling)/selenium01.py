4c334cdace085a633c63d80d341b132ba60a1c2b
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-21 13:02:30 +0900

    plus turning page function
    plus turning page function

--- pythonProject(Crawling)/selenium01.py
+++ pythonProject(Crawling)/selenium01.py
@@ -12,33 +12,39 @@ driver.find_element_by_xpath('//button[@class="btn_submit"]').click()
 driver.find_element_by_xpath('//body/div[@id="wrap"]/div[@id="header_wrap"]/div[1]/div[2]/div[1]/div[1]/ul[1]/li[2]/a[1]').click()
 driver.find_element_by_xpath('//body/div[@id="wrap"]/div[@id="container"]/div[@id="content"]/div[@id="main_pack"]/div[@id="snb"]/div[1]/div[1]/div[1]/a[2]').click()
 
-#span
+cnt = 10
 span_list = []
-span_list = driver.find_elements_by_xpath('//span[@class="info"]')
 span_temp = []
-for i in span_list:
-    span_temp.append(i.text)
-
-#press
 press_list = []
-press_list = driver.find_elements_by_xpath('//a[@class="info press"]')
-press_temp =[]
-for i in press_list:
-    press_temp.append(i.text)
-
-#title
+press_temp = []
 title_list = []
-title_list = driver.find_elements_by_xpath('//a[@class="news_tit"]')
 title_temp = []
-for i in title_list:
-    title_temp.append(i.text)
-
-#context
 context_list = []
-context_list = driver.find_elements_by_xpath('//body/div[@id="wrap"]/div[@id="container"]/div[@id="content"]/div[@id="main_pack"]/section[1]/div[1]/div[2]/ul[1]/li/div[1]/div[1]/div[2]/div[1]/a[1]')
 context_temp = []
-for i in context_list:
-    context_temp.append(i.text)
+
+while(cnt!=0):
+    #span
+    span_list = driver.find_elements_by_xpath('//span[@class="info"]')
+    for span in span_list:
+        span_temp.append(span.text)
+
+    #press
+    press_list = driver.find_elements_by_xpath('//a[@class="info press"]')
+    for press in press_list:
+        press_temp.append(press.text)
+
+    #title
+    title_list = driver.find_elements_by_xpath('//a[@class="news_tit"]')
+    for title in title_list:
+         title_temp.append(title.text)
+
+    #context
+    context_list = driver.find_elements_by_xpath('//body/div[@id="wrap"]/div[@id="container"]/div[@id="content"]/div[@id="main_pack"]/section[1]/div[1]/div[2]/ul[1]/li/div[1]/div[1]/div[2]/div[1]/a[1]')
+    for context in context_list:
+        context_temp.append(context.text)
+
+    driver.find_element_by_xpath('//i[contains(text(),"다음")]').click()
+    cnt -= 1
 
 temp = [[] for j in range(len(span_temp))]
 
