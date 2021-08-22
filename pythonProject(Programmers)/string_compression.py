ef231fb3cabdc63bbe32321f4aff09b5d4d26d2b
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-22 23:07:35 +0900

    complete string_compression.py
    complete string_compression.py

--- pythonProject(Programmers)/string_compression.py
+++ pythonProject(Programmers)/string_compression.py
@@ -1,6 +1,4 @@
 def solution(s):
-    answer = 0
-
     #split list
     s_list = [[] for j in range(len(s))]
     for cnt in range(1, len(s)+1):
@@ -9,33 +7,48 @@ def solution(s):
             s_list[cnt-1].append(s[i:i+cnt])
             i += cnt
 
+    answer_list = []
+
     #compress
-    c_list = [[] for j in range(len(s_list[i]))]
+    c_list = [[] for j in range(len(s_list))]
     for i in range(len(s_list)):
-        for j in range(len(s_list[i])):
-            # for k in range(j, len(s_list[i])-j):
+        j = 0
+        while(j != len(s_list[i])):
             k = j
             cnt = 0
-            while k != len(s_list[i]):
-                print(i, j, k, cnt, s_list)
+
+            while(k != len(s_list[i])):
                 if s_list[i][j] == s_list[i][k]:
-                    cnt +=1
+                    cnt += 1
 
                 if s_list[i][j] != s_list[i][k]:
-                    c_list[i].append(str(cnt)+s_list[i][j])
-                    s_list[i][k-1] = '0'
                     break
 
-                k += 1
+                k +=1
 
-    print(s_list)
-    print(c_list)
+            if cnt > 1:
+                c_list[i].append(str(cnt)+s_list[i][j])
+                j += cnt
+
+            else:
+                if j > len(s_list[i])-1 : j = len(s_list[i])-1
+                c_list[i].append(s_list[i][j])
+                j += 1
+
+        # combine c_list
+        temp = ""
+        for c in range(len(c_list[i])):
+            temp += c_list[i][c]
+
+        answer_list.append(len(temp))
+
+    answer = min(answer_list)
     return answer
 
 
 if __name__ == '__main__':
     solution("aabbaccc")
-    # solution("ababcdcdababcdcd")
-    # solution("abcabcdede")
-    # solution("abcabcabcabcdededededede")
-    # solution("xababcdcdababcdcd")
+    solution("ababcdcdababcdcd")
+    solution("abcabcdede")
+    solution("abcabcabcabcdededededede")
+    solution("xababcdcdababcdcd")
