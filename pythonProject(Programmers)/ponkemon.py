a9bc1742b83dabcb30a7859e19e06b511aac1b5a
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-25 21:51:20 +0900

    Update ponkemon.py

--- pythonProject(Programmers)/ponkemon.py
+++ pythonProject(Programmers)/ponkemon.py
@@ -1,13 +1,10 @@
 def solution(nums):
 
-    answer = 0
     uni_nums = list(set(nums))
-    print(uni_nums)
 
     if len(nums)//2 < len(uni_nums): answer = len(nums)//2
     else: answer = len(uni_nums)
 
-    print(answer)
     return answer
 
 if __name__ == '__main__':
