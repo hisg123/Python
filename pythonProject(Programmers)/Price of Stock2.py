6f1829b0b6558b863819faf9a5a703ee76c4ae52
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-08-28 21:03:04 +0900

    commit price of stock

---
+++ pythonProject(Programmers)/Price of Stock2.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+
+    while (len(prices) != 0):
+        temp = prices.popleft()
+        cnt = 0
+        for i in prices:
+            cnt += 1
+            if i < temp: break
+
+        answer.append(cnt)
+    return answer
+
+
+if __name__ == '__main__':
+    solution([1, 2, 3, 2, 3])
+    solution([3,1,1])
