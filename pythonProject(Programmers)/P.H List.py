fa34ee6a17645ad4382d83ef2759c874ea2006cb
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-09-11 18:56:17 +0900

    First Commit camouflage.py
    First Commit camouflage.py

--- pythonProject(Programmers)/P.H List.py
+++ pythonProject(Programmers)/P.H List.py
@@ -1,23 +1,27 @@
 def solution(phone_book):
     answer = True
-    phone_book = list(map(int, phone_book))
+    phone_book = list(map(int,phone_book))
     phone_book.sort()
-    phone_book = list(map(str, phone_book))
+    phone_book = list(map(str,phone_book))
+    print(phone_book)
 
-    temp = phone_book.pop(0)
-    print(phone_book, temp)
-    for i in phone_book:
-        if temp in i:
-            answer = False
-            print(answer)
-            return answer
-        if len(phone_book[0]) == len(temp) : temp = phone_book.pop(0)
-        if len(i) > len(temp): break
+    cnt = 0
+    phone_book_len = len(phone_book)
+    while cnt < phone_book_len-1:
+        temp = phone_book.pop(0)
+
+        for PH in phone_book:
+            if PH.startswith(temp):
+                answer = False
+                print(answer)
+                return answer
+
+        phone_book.append(temp)
+        cnt +=1
 
     print(answer)
     return answer
 
 if __name__ == '__main__':
-    solution(["119", "97674223", "1195524421"])
-    solution(["123","456","789"])
-    solution(["12","123","1235","567","88"])
+    solution(["97674223", "1195524421", "1195524421234"])
+    solution(["1","2","1"])
---
+++ pythonProject(Programmers)/camouflage.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+def solution(clothes):
+    answer = 0
+    p = defaultdict(list)
+    clothes_type = []
+
+    for cloth in clothes:
+        p[cloth[1]].append(cloth[0])
+        clothes_type.append(cloth[1])
+    #중복제거
+    clothes_type = list(set(clothes_type))
+    print(clothes_type)
+
+    if len(p) == 1:
+        answer = len(p[clothes_type[0]])
+
+    else:
+        temp = 1
+        for cloth_type in clothes_type:
+            temp *= len(p[cloth_type])+1
+        answer += temp - 1
+    print(answer)
+    return answer
+
+if __name__ == '__main__':
+    solution([["yellowhat", "headgear"],
+              ["bluesunglasses", "eyewear"],
+              ["sunglasses", "eyewear"],
+              ["green_turban", "headgear"],
+              ["blond","hair"],
+              ["black","hair"]])
+
+    solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]])
+    solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]])
