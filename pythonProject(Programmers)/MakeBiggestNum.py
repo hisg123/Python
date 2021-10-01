5f96e557a0648d7c80856168ecbf9cebf3c63646
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-10-01 12:17:51 +0900

    Update MakeBiggestNum.py
    Update MakeBiggestNum.py
    Using hash
    2021,10,01 - 12:17pm

--- pythonProject(Programmers)/MakeBiggestNum.py
+++ pythonProject(Programmers)/MakeBiggestNum.py
@@ -1,6 +1,6 @@
-from collections import deque
 def solution(number, k):
     answer = ''
+    init_k =  k
     num_dict = {}
     temp = []
     for i in range(len(number)):
@@ -10,51 +10,77 @@ def solution(number, k):
     print(max_pos)
     print(num_dict)
 
-    if k+1 < max_pos[0]:
-        for i in range(max_pos[0]):
-            temp.append((i, num_dict[i]))
+    for i in range(max_pos[0]):
+        temp.append((i, num_dict[i]))
 
-        temp.sort(key=lambda value: value[1])
-        while k:
+    #value 값을 기준으로 정렬
+    temp.sort(key=lambda value: value[1])
+    while k:
+        if temp:
             temp.pop(0)
             k -= 1
 
-        temp.sort(key=lambda  value: value[0])
+        else: break
+
+    #다시 key(=index) 값을 기준으로 정렬
+    if k == 0:
+        temp.sort(key=lambda value: value[0])
         print(temp)
         for index,i in temp:
             print(i)
             answer += i
 
+        #나머지 부분 붙이는 곳
         for index, i in num_dict.items():
             if index >= max_pos[0]: answer += i
+
         print(str(int(answer)))
         return str(int(answer))
 
     else:
-        temp = sorted(num_dict.items(), key=lambda value: value[1])
+        for index, i in num_dict.items():
+            if index >= max_pos[0]:
+                temp.append((index,i))
+
+        temp_dict = {}
+        for key, value in temp:
+            temp_dict[key] = value
+        print(temp_dict)
+
+        i = init_k - k
         while k:
-            temp.pop(0)
-            k -= 1
+            if i == len(number)-1:
+                while k:
+                    del(temp_dict[-1])
+                    k -= 1
+            if k == 0: break
 
-        temp.sort(key=lambda value: value[0])
-        for index, i in temp:
-            print(i)
+            if temp_dict[i] < temp_dict[i+1]:
+                del(temp_dict[i])
+                k -=1
+            i += 1
+        print(temp_dict)
+
+        for index, i in temp_dict.items():
             answer += i
-
         print(str(int(answer)))
         return str(int(answer))
 
 if __name__ == '__main__':
-    solution("1924", 2)
-    solution("1231234", 3)
-    solution("4177252841", 4)
+    # solution("1924", 2)
+    # solution("1231234", 3)
+    # solution("4177252841", 4)
     solution("19763", 2)
-    solution("87654321", 3)
-    solution("99999111", 3)
-    solution("1111", 3)
-    solution("8999", 3)
-    solution("9999991", 1)
-    solution("00000000",3)
-    solution("11111112", 3)
-    solution("0000001", 3)
-    solution("3322993843984398", 2)
+    # solution("87654321", 3)
+    # solution("99999111", 3)
+    # solution("1111", 3)
+    # solution("8999", 3)
+    # solution("9999991", 1)
+    # solution("00000000",3)
+    # solution("11111112", 3)
+    # solution("0000001", 3)
+    # solution("3322993843984398", 2)
+    # solution("8892299221", 3)
+
+
+
--- pythonProject(Programmers)/test.py
+++ pythonProject(Programmers)/test.py
@@ -1,74 +1,79 @@
-from collections import deque
 def solution(number, k):
     answer = ''
     num_dict = {}
-    temp = []
-    for i in range(len(number)):
-        num_dict[i] = number[i]
+    num_before = []
+    num_after = []
 
-    max_pos = sorted(num_dict.items(), key=lambda value: value[1], reverse=True)[0]
-    print(max_pos)
-    print(num_dict)
+    for i in range(len(number)): num_dict[i] = number[i]
+    max_pos = max(num_dict, key=num_dict.get)
 
+    # 앞대가리 손질에서 끝남
+    for key, value in num_dict.items():
+        if key < max_pos: num_before.append((key, value))
+        else: num_after.append((key, value))
 
-    for i in range(max_pos[0]):
-        temp.append((i, num_dict[i]))
-
-    #value 값을 기준으로 정렬
-    temp.sort(key=lambda value: value[1])
+    num_before.sort(key=lambda value: value[1])
     while k:
-        if temp:
-            temp.pop(0)
-            k -= 1
+        if num_before == [] and k > 0: break
+        num_before.pop(0)
+        k -= 1
+    num_before.sort(key=lambda value: value[0])
 
-        else: break
-
-    #다시 key(=index) 값을 기준으로 정렬
     if k == 0:
-        temp.sort(key=lambda value: value[0])
-        print(temp)
-        for index,i in temp:
-            print(i)
-            answer += i
-
-        #나머지 부분 붙이는 곳
-        for index, i in num_dict.items():
-            if index >= max_pos[0]: answer += i
+        for key, i in num_before: answer += i
+        for key, i in num_after: answer += i
 
         print(str(int(answer)))
         return str(int(answer))
 
-    else:
-        for index, i in num_dict.items():
-            if index >= max_pos[0]:
-                temp.append((index,i))
+    # 뒷대가리까지 손질 시작
+    i = num_after[0][0]
+    i_last = num_after[-1][0]
 
+    temp = {}
+    for key, value in num_after: temp[key] = value
+
+    flag = 0
+    while k:
+        if i == i_last:
+            flag = 1
+            break
+
+        if temp[i] < temp[i+1]:
+            del(temp[i])
+            k -= 1
+        i += 1
+    
+    # 뒷대가리 손질하다 맨마지막까지 손질 안되서 여리로 옴
+    if flag == 1:
+        temp = sorted(temp.items(), key=lambda value: value[1])
         while k:
             temp.pop(0)
             k -= 1
-
-        temp.sort(key=lambda value: value[0])
-        for index, i in temp:
-            print(i)
-            answer += i
-
+        temp = sorted(temp, key=lambda value: value[0])
+        for key,i in temp: answer += i
         print(str(int(answer)))
         return str(int(answer))
+    
+    # 뒷대가리 손질 끝
+    for key,i in temp.items(): answer += i
+    print(str(int(answer)))
+    return str(int(answer))
 
 if __name__ == '__main__':
-    # solution("1924", 2)
-    # solution("1231234", 3)
-    # solution("4177252841", 4)
-    # solution("19763", 2)
-    # solution("87654321", 3)
-    # solution("99999111", 3)
-    # solution("1111", 3)
-    # solution("8999", 3)
-    # solution("9999991", 1)
-    # solution("00000000",3)
-    # solution("11111112", 3)
-    # solution("0000001", 3)
-    #solution("3322993843984398", 2)
+    solution("1924", 2)
+    solution("1231234", 3)
+    solution("417725241", 4)
+    solution("19763", 2)
+    solution("87654321", 3)
+    solution("99999111", 3)
+    solution("1111", 3)
+    solution("8999", 3)
+    solution("9999991", 1)
+    solution("00000000",3)
+    solution("11111112", 3)
+    solution("0000001", 3)
+    solution("3322993843984398", 4)
     solution("8892299221", 3)
 
 
