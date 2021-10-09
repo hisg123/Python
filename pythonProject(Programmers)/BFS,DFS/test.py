fb52348e726103baca805331ff3b9e8e7c02318d
InKi Hong <72256768+hisg123@users.noreply.github.com>
2021-10-09 13:26:41 +0900

    Change etc
    Change etc

--- pythonProject(Programmers)/BFS,DFS/test.py
+++ pythonProject(Programmers)/BFS,DFS/test.py
@@ -1,17 +1,10 @@
-def DFS(numbers, target, depth):
-    if depth == len(numbers):
-    else:
-        if numbers[depth]:
-
-    depth += 1
-
-
 def solution(numbers, target):
-    answer = 0
-    depth = 0
-    answer = DFS(numbers, target, depth)
-    print(answer)
-    return answer
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
 
 if __name__ == '__main__':
     solution([1, 1, 1, 1, 1], 3)
