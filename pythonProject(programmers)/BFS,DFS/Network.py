6a6d5c2add44a70e01c33bc6c3e823de25cc09a9
hisg123 <72256768+hisg123@users.noreply.github.com>
2021-10-09 23:33:02 +0900

    Update Network.py

--- pythonProject(programmers)/BFS,DFS/Network.py
+++ pythonProject(programmers)/BFS,DFS/Network.py
@@ -1,25 +1,54 @@
 from collections import deque, defaultdict
-def MakeGraph(n):
+def MakeGraph(computers, n):
     graph = defaultdict(list)
 
-    num_index = 0
-    for num in n:
-        index = 0
-        num_index += 1
-        for i in num:
-            index += 1
-            if i == 1:
-                print(index, i)
-                graph[num_index].append(index)
+    # [1,1,0] -> 1: [1, 2]
+    for n_idx in range(n):
+        computer_index = 0
+        for computer in computers[n_idx]:
+            computer_index += 1
+            if computer == 1:
+                graph[n_idx+1].append(computer_index)
 
-    print("this is graph", graph)
+    #1: [1,2] -> 1: [2]
+    for key, value in graph.items():
+        for v in value:
+            if key ==  v: value.remove(v)
+
     return graph
 
+def DFS(graph, root):
+    visited = []
+    stack = deque([root])
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack.extend(reversed(graph[n]))
+
+    visited.sort()
+    print(f"visted[root = {root}]", visited)
+    answer = ''.join(str(visited))
+    return answer
+
 def solution(n, computers):
-    graph = MakeGraph(n)
-    answer = 0
+    #convert computers array to usable graph
+    graph = MakeGraph(computers, n)
+    print("graph:", graph)
+
+    #do DFS by changing root
+    temp = []
+    for n_idx in range(n):
+        root = n_idx + 1
+        temp.append(DFS(graph, root))
+
+    #delete repetive elements
+    set_temp = set(temp)
+    answer = len(list(set_temp))
     print(answer)
     return answer
 
 if __name__ == '__main__':
-    solution([[1, 1, 0], [1, 1, 0], [0, 0, 1]], 2)
+    solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+    solution(3, [[1, 1, 0], [1, 1, 1], [0, 1, 1]])
